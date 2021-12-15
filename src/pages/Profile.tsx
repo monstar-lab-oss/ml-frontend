842067ff761d876952634a1e0139b0ef634e5896
山下 仁麻 <jinma.yamashita@monstar-lab.com>
2021-12-15 18:41:34 +0900

    render error message

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,17 +1,18 @@
-import { useQuery } from "react-query";
+import { useQuery, QueryCache } from "react-query";
 import { http } from "@/utils/http";
 import { User } from "@/types/user";
 
-const useGetUserQuery = (id: number) =>
+const useGetUserQuery = ({ id }: { id: string }) =>
   useQuery("user", async () => await http.get<User>(`/user/${id}`));
 
 export const Profile = () => {
-  const { isLoading, data, isFetched } = useGetUserQuery(1);
+  const { isLoading, data, isFetched, isError } = useGetUserQuery({ id: "1" });
 
   return (
     <div>
       <div>ProfileScreen</div>
       <div>{isLoading && "Loading..."}</div>
+      <div>{isError && "Failed to fetch"}</div>
       {isFetched && data ? (
         <div>
           <div>
