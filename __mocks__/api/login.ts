6d695f857ad63d55365bf6e3d186a92fda4f4851
山下 仁麻 <jinma.yamashita@monstar-lab.com>
2021-12-15 18:41:25 +0900

    add delay to mock response

--- __mocks__/api/login.ts
+++ __mocks__/api/login.ts
@@ -3,6 +3,8 @@ import { rest } from "../rest";
 export const login = [
   rest.post("/login", (_, res, ctx) =>
     res(
+      // Delays response for 2000ms.
+      ctx.delay(1000),
       ctx.json({
         token: "QpwL5tke4Pnpja7X4",
       })
