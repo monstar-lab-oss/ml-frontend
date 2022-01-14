b29e2dc9ab2de20d6730c58c3dca6f8aed6fe115
jinmayamashita <9401060+jinmayamashita@users.noreply.github.com>
2022-01-14 15:17:12 +0900

    add separate components

---
+++ src/pages/Home.stories.tsx
@@ -0,0 +1,22 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { Home } from "./Home";
+import * as HeaderStories from "../components/molecules/Header.stories";
+
+export default {
+  title: "Example/Home",
+  component: Home,
+} as ComponentMeta<typeof Home>;
+
+const Template: ComponentStory<typeof Home> = (args) => <Home {...args} />;
+
+export const LoggedIn = Template.bind({});
+LoggedIn.args = {
+  // More on composing args: https://storybook.js.org/docs/react/writing-stories/args#args-composition
+  ...HeaderStories.LoggedIn.args,
+};
+
+export const LoggedOut = Template.bind({});
+LoggedOut.args = {
+  ...HeaderStories.LoggedOut.args,
+};
