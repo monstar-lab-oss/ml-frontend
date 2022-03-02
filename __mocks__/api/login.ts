5f94e9497ac5ac369da8b7f35169c520a80b3d17
Jinma Yamashita <9401060+jinmayamashita@users.noreply.github.com>
2022-03-02 14:42:26 +0900

    Update __mocks__/api/login.ts
    Co-authored-by: Yuki Noda <16083322+yukinoda@users.noreply.github.com>

--- __mocks__/api/login.ts
+++ __mocks__/api/login.ts
@@ -3,7 +3,7 @@ import { rest } from "../rest";
 export const login = [
   rest.post("/login", (_, res, ctx) =>
     res(
-      // Delays response for 2000ms.
+      // Delays response for 1000ms.
       ctx.delay(1000),
       ctx.json({
         token: "QpwL5tke4Pnpja7X4",
