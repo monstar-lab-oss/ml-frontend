f41290a024018736aef9808e55f516bdc735b8d3
jinmayamashita <9401060+jinmayamashita@users.noreply.github.com>
2022-04-28 15:35:03 +0900

    Ignore complaints about import react

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,26 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-}
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    // react/jsx-uses-react and react/react-in-jsx-scope rules are no longer necessary and can be turned off or removed.
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
+  },
+};
