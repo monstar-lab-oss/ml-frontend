0ad889c71c225b320501816c02f72993de31634d
jinmayamashita <jinma.yamashita@monstar-lab.com>
2022-06-23 17:27:40 +0900

    chore: add react/jsx-filename-extension to rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,5 +28,6 @@ module.exports = {
     "react/jsx-uses-react": "off",
     "react/react-in-jsx-scope": "off",
     "@typescript-eslint/no-unused-vars": "error",
+    "react/jsx-filename-extension": ["error", { extensions: [".jsx", ".tsx"] }],
   },
 };
