153e10aa15e148df0e18cbf6c8e38f6708533e9d
jinmayamashita <jinmayamashita@users.noreply.github.com>
2022-07-14 13:29:48 +0900

    post employee on page

--- src/pages/Employee.tsx
+++ src/pages/Employee.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { http } from "@/utils/http";
-import { useQuery } from "react-query";
+import { useForm } from "react-hook-form";
+import { useQuery, useMutation } from "react-query";
 import { Employee as Payload } from "@/types/employee";
 
 // TODO: not yet implemented
@@ -10,7 +11,37 @@ const useGetEmployeeQuery = ({ id }: { id: string }) =>
 const useGetEmployeeListQuery = () =>
   useQuery("userList", async () => await http.get<Payload[]>("/employee"));
 
+const createEmployee = async (payload: Payload) => {
+  const response = await http.post<{ message: string }>("/employee", payload);
+  return response;
+};
+
 // components
+type EmployeeFormProps = {
+  values?: Payload;
+  onSubmit: (payload: Payload) => void;
+  onCancel: () => void;
+};
+const EmployeeForm = ({ values, onSubmit, onCancel }: EmployeeFormProps) => {
+  const { register, handleSubmit } = useForm<Payload>({
+    mode: "onChange",
+    defaultValues: useMemo(() => values || {}, [values]),
+  });
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input placeholder="id" {...register("id")} />
+        <input placeholder="name" {...register("name")} />
+        <input type="submit" value="Submit" />
+        <button type="button" onClick={onCancel}>
+          Cancel
+        </button>
+      </form>
+    </>
+  );
+};
+
 type EmployeeListProps = {
   data: Payload[];
 };
@@ -26,6 +57,14 @@ const Employee = () => {
   const [isVisibleForm, setIsVisibleForm] = useState(false);
   const { isLoading, data, isFetched, isError } = useGetEmployeeListQuery();
 
+  const { mutate } = useMutation(createEmployee, {
+    onSuccess: ({ message }) => alert(message),
+    onError: (error) => alert(error),
+  });
+
+  const onSubmit = (data: Payload) => {
+    mutate(data);
+  };
   return (
     <div>
       <div>EmployeeScreen</div>
@@ -37,6 +76,15 @@ const Employee = () => {
       <div>{isLoading && "Loading..."}</div>
       <div>{isError && "Failed to fetch"}</div>
       {isFetched && data ? <EmployeeList data={data} /> : null}
+      {isVisibleForm && (
+        <>
+          <div>EmployeeForm</div>
+          <EmployeeForm
+            onSubmit={onSubmit}
+            onCancel={() => setIsVisibleForm(false)}
+          />
+        </>
+      )}
     </div>
   );
 };
