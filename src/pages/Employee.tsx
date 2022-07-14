49fed885d89386225f08e99fecfebabecf499486
jinmayamashita <jinmayamashita@users.noreply.github.com>
2022-07-14 13:29:19 +0900

    get employee list on page

--- src/pages/Employee.tsx
+++ src/pages/Employee.tsx
@@ -1,13 +1,44 @@
-import { useTranslation } from "react-i18next";
+import { useState } from "react";
+import { http } from "@/utils/http";
+import { useQuery } from "react-query";
+import { Employee as Payload } from "@/types/employee";
+
+// TODO: not yet implemented
+const useGetEmployeeQuery = ({ id }: { id: string }) =>
+  useQuery("user", async () => await http.get<Payload>(`/employee/${id}`));
+
+const useGetEmployeeListQuery = () =>
+  useQuery("userList", async () => await http.get<Payload[]>("/employee"));
+
+// components
+type EmployeeListProps = {
+  data: Payload[];
+};
+const EmployeeList = ({ data }: EmployeeListProps) => (
+  <ul>
+    {data.map(({ id, name }) => (
+      <li key={id}>{name}</li>
+    ))}
+  </ul>
+);
 
 const Employee = () => {
-  const { t } = useTranslation();
+  const [isVisibleForm, setIsVisibleForm] = useState(false);
+  const { isLoading, data, isFetched, isError } = useGetEmployeeListQuery();
 
   return (
-    <>
-      <h2>Employee</h2>
-      <p>{t("title")}</p>
-    </>
+    <div>
+      <div>EmployeeScreen</div>
+      {!isVisibleForm && (
+        <button onClick={() => setIsVisibleForm(true)}>
+          Create new employee
+        </button>
+      )}
+      <div>{isLoading && "Loading..."}</div>
+      <div>{isError && "Failed to fetch"}</div>
+      {isFetched && data ? <EmployeeList data={data} /> : null}
+    </div>
   );
 };
+
 export default Employee;
