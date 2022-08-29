03e10c0ad502d6b776ed36d152369a08af947a95
jinmayamashita <jinmayamashita@users.noreply.github.com>
2022-08-29 16:58:52 +0900

    install packages

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,14 @@ const path = require("path");
 
 module.exports = {
   stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-essentials", "@storybook/addon-actions"],
+  addons: [
+    "@storybook/addon-essentials",
+    "@storybook/addon-actions",
+    "@storybook/addon-interactions",
+  ],
+  features: {
+    interactionsDebugger: true,
+  },
   framework: "@storybook/react",
   core: {
     builder: "storybook-builder-vite",
