8c2b6038f20845662bac17dfb4e091eea0ad227e
Go Nakano <go_nakano@ML-01017.local>
2022-11-25 20:02:44 +0900

    test e2e login page

---
+++ tests/e2e/Login.test.ts
@@ -0,0 +1,45 @@
+import { test, expect, Page } from "@playwright/test";
+
+let page: Page;
+
+test.beforeEach(async ({ browser }) => {
+  page = await browser.newPage();
+  await page.goto("/");
+
+  // logout first
+  const logout = await page.locator("a", { hasText: "Logout" });
+  await logout.click();
+});
+
+test.afterAll(async () => {
+  await page.close();
+});
+
+test.describe("playwright e2e test for login page", () => {
+  test("success login", async () => {
+    const email = await page.locator("#email");
+    const password = await page.locator("#password");
+    const submitButton = await page.locator("button[type=submit]");
+
+    await email.clear();
+    await email.type("eve.holt@reqres.in");
+    await password.clear();
+    await password.type("cityslicka");
+    await submitButton.click();
+
+    await expect(page).toHaveURL(/.*home/);
+  });
+
+  test("fail login", async () => {
+    const email = await page.locator("#email");
+    const submitButton = await page.locator("button[type=submit]");
+
+    await email.clear();
+    await email.type("test@123123abcabc");
+    await submitButton.click();
+    await page.waitForTimeout(5000);
+
+    await expect(page.locator("#erroremail")).toBeVisible();
+    await expect(page.locator("#errorpassword")).toBeVisible();
+  });
+});
