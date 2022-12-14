ed467c46e57a739734e5c98933aa4ef944531c58
jinmayamashita <jinmayamashita@users.noreply.github.com>
2022-12-14 15:13:09 +0900

    test: restore data-testid

--- src/pages/EmployeeList.tsx
+++ src/pages/EmployeeList.tsx
@@ -14,7 +14,7 @@ const EmployeeList = () => {
 
   return (
     <>
-      <h2>Employee</h2>
+      <h2 data-testid="employee-list-title">Employee</h2>
       <div>{isLoading && "Loading..."}</div>
       <div>{isError && "Failed to fetch"}</div>
 
