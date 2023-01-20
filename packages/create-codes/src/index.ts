dc587a4029191c19f47f882e289748a5b5a8cc43
Jinma Yamashita <9401060+jinmayamashita@users.noreply.github.com>
2023-01-20 23:18:09 +0900

    add create-codes package

---
+++ packages/create-codes/src/index.ts
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+import meow from "meow";
+
+const help = `
+  Usage:
+    $ npx create-codes [<dir>] [<lib>] [flags...]
+  Flags:
+    --help, -h          Show this help message
+    --version, -v       Show the version of this script
+`;
+
+function run() {
+  meow(help, {
+    flags: {
+      help: { type: "boolean", default: false, alias: "h" },
+      version: { type: "boolean", default: false, alias: "v" },
+    },
+  });
+}
+run();
