b40228da848fc8a35573f9d0ef6167bf911bfd10
Jinma Yamashita <9401060+jinmayamashita@users.noreply.github.com>
2023-01-23 18:31:17 +0900

    e2e testing to package

--- packages/e2e-testing/playwright.config.ts
+++ packages/e2e-testing/playwright.config.ts
@@ -2,24 +2,22 @@ import { type PlaywrightTestConfig, devices } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
   forbidOnly: !!process.env.CI,
-  testDir: "./tests/e2e",
-  webServer: {
-    command: "yarn dev",
-    port: 3000,
-  },
+  testDir: "__tests__",
   retries: process.env.CI ? 2 : 0,
-  globalSetup: require.resolve("./tests/e2e/utils/global-setup"),
+  globalSetup: require.resolve("./__tests__/utils/global-setup"),
   use: {
     headless: !!process.env.CI,
-    baseURL: "http://localhost:3000/",
     // Tell all tests to load signed-in state from 'storageState.json'.
-    storageState: "./tests/e2e/utils/storageState.json",
+    storageState: "./__tests__/utils/storageState.json",
     trace: "on-first-retry",
   },
   projects: [
     {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      name: "react",
+      use: {
+        baseURL: "http://localhost:3000",
+        ...devices["Desktop Chrome"],
+      },
     },
   ],
 };
