6dbb152e9199ceb0cf9b0f5926511f4a7897387d
Jinma Yamashita <9401060+jinmayamashita@users.noreply.github.com>
2023-02-16 10:26:29 +0900

    test: adjust tests

--- packages/create-codes/__tests__/cli.test.ts
+++ packages/create-codes/__tests__/cli.test.ts
@@ -1,25 +1,29 @@
 import { describe, test, expect, beforeAll, afterAll } from "vitest";
 import path from "node:path";
+import {
+  execFile,
+  spawn,
+  ChildProcessWithoutNullStreams,
+} from "node:child_process";
+import { promisify } from "node:util";
 import fse from "fs-extra";
-import child, { ChildProcessWithoutNullStreams } from "node:child_process";
-import util from "node:util";
-import concat from "concat-stream";
-import { spawn } from "node:child_process";
+import stripAnsi from "strip-ansi";
 
-const keys = {
+const KEYS = {
   ENTER: "\x0D",
+  SPACE: "\x20",
 };
 
-// outside monorepo
+// outside of monorepo
 const cwd = path.resolve(__dirname, "../../../..");
-
-const testDir = "my-test" as const;
-
-const exe = util.promisify(child.execFile);
-
 const createCodes = path.resolve(__dirname, "../dist/index.js");
 
-const EXPECTED_HELP = `Create a new codes for front-end app
+describe("create-codes", () => {
+  const testDir = "my-test" as const;
+
+  const exe = promisify(execFile);
+
+  const EXPECTED_HELP = `Create a new codes for front-end app
 
     Usage:
       $ npx create-codes [<dir>] [flags...]
@@ -28,73 +32,81 @@ const EXPECTED_HELP = `Create a new codes for front-end app
       --help, -h          Show this help message
       --version, -v       Show the version of this script`;
 
-describe("create-codes cli", () => {
-  beforeAll(() => cleanupTestDir());
-  afterAll(() => cleanupTestDir());
+  beforeAll(() => cleanup(testDir));
+  afterAll(() => cleanup(testDir));
 
-  // FIXME: It is broken. Implementation not yet complete, will test another way after implementation.
-  describe.skip("install react boilerplate with cli", () => {
+  describe("install boilerplate files with cli", () => {
     test("interactively configure", async () => {
       const cli = spawn("node", [createCodes], { cwd });
-      const results = await exeInteractive(cli, [
-        testDir,
-        keys.ENTER,
+      const stdout = await run(cli, testDir);
 
-        keys.ENTER,
-        keys.ENTER,
-        keys.ENTER,
-        keys.ENTER,
-        keys.ENTER,
-        keys.ENTER,
-        keys.ENTER,
-        keys.ENTER,
-        keys.ENTER,
-      ]);
+      expect(stdout).toContain(`
+┌  ≛ Create Codes :: Write less boilerplate, focus on the your front-end magic.
+│
+`);
+      expect(stdout).toContain(`
+●  Where would you like to create your app?
+│  ./my-app
+└
+`);
+      expect(stdout).toContain(`
+●  Select a JavsScript library for UI
+│  ● Solid 
+└
+`);
+      expect(stdout).toContain(`
+●  Select an API solution
+│  ● RESTful 
+│  ○ GraphQL
+└
+`);
+      expect(stdout).toContain(`
+●  Select the app features you want to use
+│  ◻ Add Router for SPA Development
+│  ◻ Add CRUD Operations 
+│  ◻ Add Zustand for State Management
+│  ◻ Add Authentication Feature
+└
+`);
+      expect(stdout).toContain(`
+●  Select the testing type that you want to use
+│  ◻ Vitest for Unit testing 
+│  ◻ Storybook for visual testing
+│  ◻ Playwright for end-to-end testing
+└
+`);
+      expect(stdout).toContain(`
+●  Select tools that help with code quality
+│  ◻ ESLint for code linting 
+│  ◻ Prettier for code formatting
+└
+`);
+      expect(stdout).toContain(`
+○  Downloading Templates files
+`);
+      expect(stdout).toContain(`
+○  Downloaded Templates files
+`);
 
-      expect(results).toContain(`Create Codes`);
-      expect(results).toContain(`Welcome!`);
-      expect(results).toContain(
-        `? Where Would You like to Create Your Application?`
-      );
-      expect(results).toContain(`? Select a JavsScript library for UI`);
-      expect(results).toContain(`? Select an API Solution`);
-      expect(results).toContain(`? Select module do you want to use`);
-      expect(results).toContain(`? Add Testing codes for Catching bugs early?`);
-      expect(results).toContain(`? Add Vitest for Unit Testing?`);
-      expect(results).toContain(`? Add Storybook for Visual Testing?`);
-      expect(results).toContain(`? Add Playwright for End-To-End Testing?`);
-      expect(results).toContain(`? Add ESLint for Code Linting?`);
-      expect(results).toContain(`? Add Prettier for Code Formatting?`);
-      expect(results).toContain(`Success! Created a new app at "my-test".`);
+      expect(stdout).toContain(`
+○  Success! Created a new app at "${testDir}" ─────────────────╮
+│                                                           │
+│  Inside this directory, you can run:                      │
+│                                                           │
+│    pnpm dev                                               │
+│      Develop your app with development server             │
+│                                                           │
+│    pnpm build                                             │
+│      Build directory with a production build of your app  │
+│                                                           │
+├───────────────────────────────────────────────────────────╯
+│
+└  Happy hacking!
+`);
     });
-  });
 
-  // TODO: Skip testing as it is not yet implemented.
-  describe.skip("install react boilerplate to specify dir", () => {
-    test("install", async () => {
-      await exe("node", [createCodes, testDir], { cwd });
-
-      const expectDirs = [
-        ".env.template",
-        ".eslintrc.js",
-        ".gitignore",
-        "README.md",
-        "__mocks__",
-        "babel.config.js",
-        "index.html",
-        "jest-setup.ts",
-        "package.json",
-        "public",
-        "src",
-        "tests",
-        "tsconfig.json",
-        "vite.config.ts",
-        "yarn.lock",
-      ];
-
-      expect(fse.readdirSync(path.resolve(cwd, testDir))).toStrictEqual(
-        expectDirs
-      );
+    test.todo("installed boilerplate file is structured as intended", () => {
+      //TODO:
     });
   });
 
@@ -125,45 +137,48 @@ describe("create-codes cli", () => {
   });
 });
 
-function cleanupTestDir() {
-  const installedTestDir = path.resolve(cwd, testDir);
-
-  fse.existsSync(installedTestDir) &&
-    fse.rmSync(installedTestDir, { recursive: true });
-}
-// FIXME: Displaying loading in the process of cli execution cause test errors.
-function exeInteractive(
-  cli: ChildProcessWithoutNullStreams,
-  inputs: string[] = [],
-  delay: number = 200
-) {
-  let currentInputTimeout: NodeJS.Timeout;
-
-  cli.stdin.setDefaultEncoding("utf-8");
-
-  const loop = (inputs: string[]) => {
-    if (!inputs.length) return void cli.stdin.end();
-
-    currentInputTimeout = setTimeout(() => {
-      cli.stdin.write(inputs[0]);
-      loop(inputs.slice(1));
-    }, delay);
-  };
+async function run(cli: ChildProcessWithoutNullStreams, testDir: string) {
+  let acc = "";
 
   return new Promise((resolve, reject) => {
-    cli.stderr.once("data", (err) => {
-      cli.stdin.end();
+    cli.stdout.on("data", async (data) => {
+      acc += data.toString("utf-8");
 
-      if (currentInputTimeout) clearTimeout(currentInputTimeout);
-      reject(err.toString());
+      if (acc.match(/Where would you like to create your app/)) {
+        cli.stdin.write(Buffer.from(testDir, "utf-8"));
+        cli.stdin.write(KEYS.ENTER);
+      }
+
+      if (acc.match(/Select a JavsScript library for UI/)) {
+        cli.stdin.write(KEYS.ENTER);
+      }
+
+      if (acc.match(/Select an API solution/)) {
+        cli.stdin.write(KEYS.ENTER);
+      }
+
+      if (acc.match(/Select the app features you want to use/)) {
+        cli.stdin.write(KEYS.SPACE);
+        cli.stdin.write(KEYS.ENTER);
+      }
+
+      if (acc.match(/Select the testing type that you want to use/)) {
+        cli.stdin.write(KEYS.SPACE);
+        cli.stdin.write(KEYS.ENTER);
+      }
+
+      if (acc.match(/Select tools that help with code quality/)) {
+        cli.stdin.write(KEYS.SPACE);
+        cli.stdin.write(KEYS.ENTER);
+      }
     });
 
-    cli.on("error", reject);
-
-    loop(inputs);
-
-    cli.stdout.pipe(
-      concat((result: Buffer) => resolve(result.toString("utf-8")))
-    );
+    cli.on("exit", () => resolve(stripAnsi(acc)));
+    cli.on("error", (e) => reject(e));
   });
+}
+
+function cleanup(dir: string) {
+  const installedDir = path.resolve(cwd, dir);
+  fse.existsSync(installedDir) && fse.rmSync(installedDir, { recursive: true });
 }
