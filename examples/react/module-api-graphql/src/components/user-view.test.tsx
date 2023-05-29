4800e7b11801394f9ed1b61ba37c6544f8310b3e
Patrick Dan Lacuna <contactme@ptrklacuna.com>
2023-05-29 17:44:03 +0900

    Add unit test for React GraphQL module

---
+++ examples/react/module-api-graphql/src/components/user-view.test.tsx
@@ -0,0 +1,25 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import { UserView } from "./user-view";
+import { PropsWithChildren } from "react";
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { retry: false } },
+});
+
+const wrapper = (props: PropsWithChildren) => (
+  <QueryClientProvider client={queryClient}>
+    {props.children}
+  </QueryClientProvider>
+);
+
+test("Should return user when page loaded", async () => {
+  render(<UserView />, { wrapper });
+
+  await waitForElementToBeRemoved(() => screen.queryByLabelText("loading"));
+  expect(screen.getByRole("heading", { name: "John", level: 2 })).toBeDefined();
+});
---
+++ examples/react/testing-vitest/vitest.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: "jsdom",
+    setupFiles: path.resolve(__dirname, "vitest.setup.ts"),
+  },
+});
