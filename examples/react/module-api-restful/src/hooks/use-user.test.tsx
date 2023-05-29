a38ce22f89483dcb3d8367f1529bd15dcede9936
Patrick Dan Lacuna <contactme@ptrklacuna.com>
2023-05-29 20:14:00 +0900

    Add unit test for React RESTful module

---
+++ examples/react/module-api-restful/src/hooks/use-user.test.tsx
@@ -0,0 +1,29 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { renderHook, waitFor } from "@testing-library/react";
+import { PropsWithChildren } from "react";
+import { useUser } from "./use-user";
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { retry: false } },
+});
+
+const wrapper = (props: PropsWithChildren) => (
+  <QueryClientProvider client={queryClient}>
+    {props.children}
+  </QueryClientProvider>
+);
+
+test("Should fetch user and set loading and error state", async () => {
+  const { result } = renderHook(useUser, { wrapper });
+
+  expect(result.current.isLoading).toBe(true);
+
+  await waitFor(() => {
+    expect(result.current.isLoading).toBe(false);
+  });
+
+  expect(result.current.data).toEqual({
+    id: "f79e82e8-c34a-4dc7-a49e-9fadc0979fda",
+    name: "John",
+  });
+});
