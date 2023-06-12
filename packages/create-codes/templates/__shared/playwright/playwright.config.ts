86ab1034c79327f250bedf4c0c530d9da82b4676
Patrick Dan Lacuna <contactme@ptrklacuna.com>
2023-06-12 17:20:23 +0900

    Add global setup to playwright config template

--- packages/create-codes/templates/__shared/playwright/playwright.config.ts
+++ packages/create-codes/templates/__shared/playwright/playwright.config.ts
@@ -1,10 +1,12 @@
 import { PlaywrightTestConfig, devices } from "@playwright/test";
+import { resolve } from "node:path";
 
 const CI = !!process.env.CI;
 
 const config: PlaywrightTestConfig = {
   forbidOnly: CI,
   retries: CI ? 2 : 0,
+  globalSetup: resolve("./__tests__/utils/global-setup"),
   testDir: "__tests__",
   use: {
     baseURL: "http://localhost:3000",
