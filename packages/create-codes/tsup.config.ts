25642d1f293288a1a90ce818b332cabdd298cc61
Jinma Yamashita <9401060+jinmayamashita@users.noreply.github.com>
2023-06-15 08:53:18 +0900

    use ref name from env

--- packages/create-codes/tsup.config.ts
+++ packages/create-codes/tsup.config.ts
@@ -4,5 +4,8 @@ export default defineConfig((options: Options) => ({
   entry: ["src/index.ts"],
   format: ["cjs"],
   clean: true,
+  define: {
+    "process.env.REF_NAME": `"${process.env.REF_NAME}"`,
+  },
   ...options,
 }));
