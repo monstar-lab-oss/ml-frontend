d90e4bbfa7fb2349f3f68b7d0473998e26919f01
Nozomi Igarashi <nozomi.igarashi@monstar-lab.com>
2023-07-20 19:31:30 +0900

    fix unit test

--- src/pages/Employee.test.tsx
+++ src/pages/Employee.test.tsx
@@ -9,18 +9,18 @@ describe('Employee', () => {
     expect(getByTestId('employee-list-title')).toBeInTheDocument()
   })
 
-  test('renders employee create page when path is /employees/new', () => {
+  test('renders employee create page when path is /ml-frontend/employees/new', () => {
     const { getByTestId } = renderWithQueryClient(<Employee />)
 
-    act(() => history.replaceState(null, "", "/employees/new"));
+    act(() => history.replaceState(null, "", "/ml-frontend/employees/new"));
 
     expect(getByTestId('employee-create-title')).toBeInTheDocument()
   })
 
-  test('renders employee update page when path is /employees/:id', async () => {
+  test('renders employee update page when path is /ml-frontend/employees/:id', async () => {
     const { getByTestId } = renderWithQueryClient(<Employee />)
 
-    act(() => history.replaceState(null, "", "/employees/1"));
+    act(() => history.replaceState(null, "", "/ml-frontend/employees/1"));
 
     await waitFor(() => {
       expect(getByTestId('employee-update-title')).toBeInTheDocument()
