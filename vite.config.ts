620afad9ccfc5835c1f6a263928b4551d3e6bcdb
Nozomi Igarashi <nozomi.igarashi@monstar-lab.com>
2023-07-20 18:36:14 +0900

    fix vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'ml-frontend',
   resolve: {
     alias: {
       "@/__mocks__": path.resolve(__dirname, "./__mocks__"),
