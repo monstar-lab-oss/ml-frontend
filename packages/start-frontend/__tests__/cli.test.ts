24b2ccb7847052a5d495f21a57e9be177e37e8c3
Jinma <9401060+jinmayamashita@users.noreply.github.com>
2024-05-17 12:59:56 +0900

    test: improve cli test

--- packages/start-frontend/__tests__/cli.test.ts
+++ packages/start-frontend/__tests__/cli.test.ts
@@ -1,168 +1,124 @@
 import { describe, test, expect, beforeAll, afterAll } from "vitest";
 import path from "node:path";
-import fse from "fs-extra";
-import child, { ChildProcessWithoutNullStreams } from "node:child_process";
 import util from "node:util";
+import { execFile, spawn } from "node:child_process";
+import fse from "fs-extra";
 import concat from "concat-stream";
-import { spawn } from "node:child_process";
 
+// variables
 const keys = {
   ENTER: "\x0D",
   DOWN: "\u001B\u005B\u0042",
+  SPACE: "\x20",
 };
 
-// outside monorepo
 const cwd = path.resolve(__dirname, "../../../..");
-
 const testDir = "my-test" as const;
+const cli = path.resolve(__dirname, "..", "dist", "index.js");
 
-const exe = util.promisify(child.execFile);
+// helpers
+const exe = util.promisify(execFile);
 
-const startFrontend = path.resolve(__dirname, "../dist/index.js");
+async function cleanupTestDir() {
+  const installedTestDir = path.resolve(cwd, testDir);
 
-const EXPECTED_HELP = `Create a new codes for front-end app
+  fse.existsSync(installedTestDir) &&
+    fse.rmSync(installedTestDir, { recursive: true });
+}
+
+type RunCLIOptions = {
+  inputs?: string[];
+  delay?: number;
+};
+function runCLIWithInputs(cliPath: string, opts: RunCLIOptions) {
+  const cliProcess = spawn("node", [cliPath], { cwd });
+  const { inputs = [], delay = 500 } = opts;
+
+  let currentInputTimeout: NodeJS.Timeout;
+
+  function loop(inputs: string[]) {
+    if (!inputs.length) {
+      clearTimeout(currentInputTimeout);
+      return cliProcess.stdin.end();
+    }
+
+    currentInputTimeout = setTimeout(() => {
+      cliProcess.stdin.write(inputs[0]);
+
+      loop(inputs.slice(1));
+    }, delay);
+  }
+
+  return new Promise((resolve) => {
+    loop(inputs);
+
+    cliProcess.stdout.pipe(
+      concat((result: Buffer) => resolve(result.toString("utf-8")))
+    );
+  });
+}
+
+// tests
+describe("start-frontend cli", async () => {
+  beforeAll(() => cleanupTestDir());
+  afterAll(() => cleanupTestDir());
+
+  test("--version flag works", async () => {
+    const { stdout } = await exe("node", [cli, "--version"]);
+    expect(stdout.trim()).toMatch(/^(\d+\.)?(\d+\.)?(\*|\d+)$/);
+  });
+
+  test("-v flag works", async () => {
+    const { stdout } = await exe("node", [cli, "-v"]);
+    expect(stdout.trim()).toMatch(/^(\d+\.)?(\d+\.)?(\*|\d+)$/);
+  });
+
+  test("--help flag works", async () => {
+    const { stdout } = await exe("node", [cli, "-h"]);
+    expect(stdout.trim()).toBe(`Create a new codes for front-end app
 
     Usage:
       $ npx start-frontend [<dir>] [flags...]
 
     Flags:
       --help, -h          Show this help message
-      --version, -v       Show the version of this script`;
-
-describe("start-frontend cli", () => {
-  beforeAll(() => cleanupTestDir());
-  afterAll(() => cleanupTestDir());
-
-  describe("install react boilerplate with cli", () => {
-    test("interactively configure", async () => {
-      const cli = spawn("node", [startFrontend], { cwd });
-      const results = await exeInteractive(cli, [
-        testDir,
-        keys.ENTER,
-        keys.ENTER,
-        keys.ENTER,
-        keys.ENTER,
-        keys.ENTER,
-        keys.ENTER,
-        keys.ENTER,
-        keys.ENTER,
-        keys.ENTER,
-        keys.ENTER,
-      ]);
-
-      expect(results).toContain(`start-frontend`);
-      expect(results).toContain(`Welcome!`);
-      expect(results).toContain(
-        `? Where Would You like to Create Your Application?`
-      );
-      expect(results).toContain(`? Select a JavsScript library for UI`);
-      expect(results).toContain(`? Select an API Solution`);
-      expect(results).toContain(`? Select module do you want to use`);
-      expect(results).toContain(`? Add Testing codes for Catching bugs early?`);
-      expect(results).toContain(`? Add Vitest for Unit Testing?`);
-      expect(results).toContain(`? Add Storybook for Visual Testing?`);
-      expect(results).toContain(`? Add Playwright for End-To-End Testing?`);
-      expect(results).toContain(`? Add ESLint for Code Linting?`);
-      expect(results).toContain(`? Add Prettier for Code Formatting?`);
-      expect(results).toContain(`Success! Created a new app at "my-test".`);
-    });
+      --version, -v       Show the version of this script`);
   });
 
-  // TODO: Skip testing as it is not yet implemented.
-  describe.skip("install react boilerplate to specify dir", () => {
-    test("install", async () => {
-      await exe("node", [startFrontend, testDir], { cwd });
+  test("-h flag works", async () => {
+    const { stdout } = await exe("node", [cli, "-h"]);
+    expect(stdout.trim()).toBe(`Create a new codes for front-end app
 
-      const expectDirs = [
-        ".env.template",
-        ".eslintrc.js",
-        ".gitignore",
-        "README.md",
-        "__mocks__",
-        "babel.config.js",
-        "index.html",
-        "jest-setup.ts",
-        "package.json",
-        "public",
-        "src",
-        "tests",
-        "tsconfig.json",
-        "vite.config.ts",
-        "yarn.lock",
-      ];
+    Usage:
+      $ npx start-frontend [<dir>] [flags...]
 
-      expect(fse.readdirSync(path.resolve(cwd, testDir))).toStrictEqual(
-        expectDirs
-      );
-    });
+    Flags:
+      --help, -h          Show this help message
+      --version, -v       Show the version of this script`);
   });
 
-  describe("printing help message", () => {
-    test("--help flag works", async () => {
-      const { stdout } = await exe("node", [startFrontend, "--help"]);
-      expect(stdout.trim()).toBe(EXPECTED_HELP);
-    });
-
-    test("-h flag works", async () => {
-      const { stdout } = await exe("node", [startFrontend, "-h"]);
-      expect(stdout.trim()).toBe(EXPECTED_HELP);
-    });
-  });
-
-  describe("printing version", () => {
-    test("--version flag works", async () => {
-      const { stdout } = await exe("node", [startFrontend, "--version"]);
-      // eslint-disable-next-line turbo/no-undeclared-env-vars
-      expect(stdout.trim()).toBe(process.env.npm_package_version);
-    });
-
-    test("-v flag works", async () => {
-      const { stdout } = await exe("node", [startFrontend, "-v"]);
-      // eslint-disable-next-line turbo/no-undeclared-env-vars
-      expect(stdout.trim()).toBe(process.env.npm_package_version);
+  test("handle interactive configuration on CLI", async () => {
+    await runCLIWithInputs(cli, {
+      inputs: [
+        // Where Would You like to Create Your Application?
+        keys.ENTER,
+        // Select a JavsScript library for UI (Use arrow keys)
+        keys.ENTER,
+        // Select an API Solution (Use arrow keys)
+        keys.ENTER,
+        // Select module do you want to use
+        keys.ENTER,
+        // Add Testing codes for Catching bugs early?
+        keys.ENTER,
+        // Add Vitest for Unit Testing?
+        keys.ENTER,
+        // Add Storybook for Visual Testing?
+        keys.ENTER,
+        // Add Playwright for End-To-End Testing?
+        keys.ENTER,
+        // Add Prettier for Code Formatting?
+        keys.ENTER,
+      ],
     });
   });
 });
-
-function cleanupTestDir() {
-  const installedTestDir = path.resolve(cwd, testDir);
-
-  fse.existsSync(installedTestDir) &&
-    fse.rmSync(installedTestDir, { recursive: true });
-}
-// FIXME: Displaying loading in the process of cli execution cause test errors.
-function exeInteractive(
-  cli: ChildProcessWithoutNullStreams,
-  inputs: string[] = [],
-  delay: number = 200
-) {
-  let currentInputTimeout: NodeJS.Timeout;
-
-  cli.stdin.setDefaultEncoding("utf-8");
-
-  const loop = (inputs: string[]) => {
-    if (!inputs.length) return void cli.stdin.end();
-
-    currentInputTimeout = setTimeout(() => {
-      cli.stdin.write(inputs[0]);
-      loop(inputs.slice(1));
-    }, delay);
-  };
-
-  return new Promise((resolve, reject) => {
-    cli.stderr.once("data", (err) => {
-      cli.stdin.end();
-
-      if (currentInputTimeout) clearTimeout(currentInputTimeout);
-      reject(err.toString());
-    });
-
-    cli.on("error", reject);
-
-    loop(inputs);
-
-    cli.stdout.pipe(
-      concat((result: Buffer) => resolve(result.toString("utf-8")))
-    );
-  });
-}
