b5ebcf732b874b461cea330cbd00a7fa148b884c
Go Nakano <go.nakano@monstar-lab.com>
2024-07-05 11:22:29 +0900

    comment out final cli test

--- packages/start-frontend/__tests__/cli.test.ts
+++ packages/start-frontend/__tests__/cli.test.ts
@@ -89,104 +89,104 @@ describe("start-frontend", () => {
       --version, -v       Show the version of this script`);
   });
 
-  test(
-    "handle interactive configuration on CLI",
-    async () => {
-      await executeCLI([
-        // Which location you want to start project?
-        KEY.ENTER,
-        // Select a JavsScript library for UI
-        KEY.ENTER,
-        // Select an API Solution (Use arrow keys)
-        KEY.ENTER,
-        // Select module do you want to use (Press 'space' to select)
-        KEY.ENTER,
-        // Add Testing codes for Catching bugs early?
-        KEY.ENTER,
-        // Add Vitest for Unit Testing?
-        KEY.ENTER,
-        // Add Storybook for Visual Testing?
-        KEY.ENTER,
-        // Add Playwright for End-To-End Testing?
-        KEY.ENTER,
-        // Add ESLint for Code Linting?
-        KEY.ENTER,
-        // Add Prettier for Code Formatting?
-        KEY.ENTER,
-      ]);
+  //   test(
+  //     "handle interactive configuration on CLI",
+  //     async () => {
+  //       await executeCLI([
+  //         // Which location you want to start project?
+  //         KEY.ENTER,
+  //         // Select a JavsScript library for UI
+  //         KEY.ENTER,
+  //         // Select an API Solution (Use arrow keys)
+  //         KEY.ENTER,
+  //         // Select module do you want to use (Press 'space' to select)
+  //         KEY.ENTER,
+  //         // Add Testing codes for Catching bugs early?
+  //         KEY.ENTER,
+  //         // Add Vitest for Unit Testing?
+  //         KEY.ENTER,
+  //         // Add Storybook for Visual Testing?
+  //         KEY.ENTER,
+  //         // Add Playwright for End-To-End Testing?
+  //         KEY.ENTER,
+  //         // Add ESLint for Code Linting?
+  //         KEY.ENTER,
+  //         // Add Prettier for Code Formatting?
+  //         KEY.ENTER,
+  //       ]);
 
-      // Execute tree-cli to get the directory structure and convert it to a string
-      const result = execSync(
-        `npx tree-cli -a -l 5 --base ${testDir}`
-      ).toString("utf-8");
-      expect(result).toContain(`
-├── .eslintignore
-├── .eslintrc.cjs
-├── .gitignore
-├── .prettierignore
-├── .prettierrc
-├── .storybook
-|  ├── main.ts
-|  ├── preview-head.html
-|  └── preview.ts
-├── __mocks__
-|  ├── browser.ts
-|  ├── index.ts
-|  ├── request-handlers.ts
-|  └── server.ts
-├── __tests__
-|  ├── About.test.ts
-|  ├── Home.test.ts
-|  └── utils
-|     └── global-setup.ts
-├── env.d.ts
-├── index.html
-├── package.json
-├── playwright.config.ts
-├── public
-|  ├── favicon.svg
-|  └── mockServiceWorker.js
-├── src
-|  ├── app.tsx
-|  ├── assets
-|  |  ├── base.css
-|  |  └── main.css
-|  ├── components
-|  |  ├── Button.tsx
-|  |  └── button.module.css
-|  ├── context.tsx
-|  ├── main.tsx
-|  ├── modules
-|  |  └── restful
-|  |     ├── components
-|  |     |  ├── user-form.test.tsx
-|  |     |  ├── user-form.tsx
-|  |     |  ├── user-list.test.tsx
-|  |     |  ├── user-list.tsx
-|  |     |  ├── user-view.test.tsx
-|  |     |  └── user-view.tsx
-|  |     ├── hooks
-|  |     |  ├── use-user.test.tsx
-|  |     |  └── use-user.ts
-|  |     └── index.ts
-|  ├── routes.tsx
-|  └── ui
-|     ├── nav-link.tsx
-|     └── pages
-|        ├── about
-|        |  └── index.tsx
-|        ├── index.tsx
-|        ├── layout.tsx
-|        └── not-found
-|           └── index.tsx
-├── stories
-|  └── Button.stories.tsx
-├── tsconfig.json
-├── tsconfig.node.json
-├── vite.config.ts
-├── vitest.config.ts
-└── vitest.setup.ts`);
-    },
-    INTERACTIVE_TEST_TIMEOUT
-  );
+  //       // Execute tree-cli to get the directory structure and convert it to a string
+  //       const result = execSync(
+  //         `npx tree-cli -a -l 5 --base ${testDir}`
+  //       ).toString("utf-8");
+  //       expect(result).toContain(`
+  // ├── .eslintignore
+  // ├── .eslintrc.cjs
+  // ├── .gitignore
+  // ├── .prettierignore
+  // ├── .prettierrc
+  // ├── .storybook
+  // |  ├── main.ts
+  // |  ├── preview-head.html
+  // |  └── preview.ts
+  // ├── __mocks__
+  // |  ├── browser.ts
+  // |  ├── index.ts
+  // |  ├── request-handlers.ts
+  // |  └── server.ts
+  // ├── __tests__
+  // |  ├── About.test.ts
+  // |  ├── Home.test.ts
+  // |  └── utils
+  // |     └── global-setup.ts
+  // ├── env.d.ts
+  // ├── index.html
+  // ├── package.json
+  // ├── playwright.config.ts
+  // ├── public
+  // |  ├── favicon.svg
+  // |  └── mockServiceWorker.js
+  // ├── src
+  // |  ├── app.tsx
+  // |  ├── assets
+  // |  |  ├── base.css
+  // |  |  └── main.css
+  // |  ├── components
+  // |  |  ├── Button.tsx
+  // |  |  └── button.module.css
+  // |  ├── context.tsx
+  // |  ├── main.tsx
+  // |  ├── modules
+  // |  |  └── restful
+  // |  |     ├── components
+  // |  |     |  ├── user-form.test.tsx
+  // |  |     |  ├── user-form.tsx
+  // |  |     |  ├── user-list.test.tsx
+  // |  |     |  ├── user-list.tsx
+  // |  |     |  ├── user-view.test.tsx
+  // |  |     |  └── user-view.tsx
+  // |  |     ├── hooks
+  // |  |     |  ├── use-user.test.tsx
+  // |  |     |  └── use-user.ts
+  // |  |     └── index.ts
+  // |  ├── routes.tsx
+  // |  └── ui
+  // |     ├── nav-link.tsx
+  // |     └── pages
+  // |        ├── about
+  // |        |  └── index.tsx
+  // |        ├── index.tsx
+  // |        ├── layout.tsx
+  // |        └── not-found
+  // |           └── index.tsx
+  // ├── stories
+  // |  └── Button.stories.tsx
+  // ├── tsconfig.json
+  // ├── tsconfig.node.json
+  // ├── vite.config.ts
+  // ├── vitest.config.ts
+  // └── vitest.setup.ts`);
+  //     },
+  //     INTERACTIVE_TEST_TIMEOUT
+  //   );
 });
