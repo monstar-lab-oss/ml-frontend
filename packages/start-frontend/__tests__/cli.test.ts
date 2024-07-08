350419b67f1777ea2447bde1fbb86937ba5caca3
Go Nakano <go.nakano@monstar-lab.com>
2024-07-08 14:27:42 +0900

    fix test

--- packages/start-frontend/__tests__/cli.test.ts
+++ packages/start-frontend/__tests__/cli.test.ts
@@ -1,21 +1,22 @@
 import { describe, test, expect, beforeAll, afterAll } from "vitest";
 import path from "node:path";
 import util from "node:util";
-import { execFile, execSync } from "node:child_process";
+import { exec, execFile, execSync } from "node:child_process";
 import fse from "fs-extra";
 
 const START_FRONTEND = path.resolve(__dirname, "..", "dist", "index.js");
 
 console.log("runner temp path", process.env.RUNNER_TEMP);
+console.log("START_FRONTEND", START_FRONTEND);
 
-// const KEY = {
-//   ENTER: "\x0D",
-//   DOWN: "\u001B\u005B\u0042",
-//   SPACE: "\x20",
-// };
+const KEY = {
+  ENTER: "\x0D",
+  DOWN: "\u001B\u005B\u0042",
+  SPACE: "\x20",
+};
 
 // Timeout duration for interactive tests, to allow for code stub downloads
-// const INTERACTIVE_TEST_TIMEOUT = 20000;
+const INTERACTIVE_TEST_TIMEOUT = 20000;
 
 let testDir = "my-test";
 
@@ -25,23 +26,23 @@ async function cleanupTestDir() {
   fse.existsSync(testDir) && fse.rmSync(testDir, { recursive: true });
 }
 
-// async function executeCLI(inputs: string[], delay = 500) {
-//   const cliProcess = exec(`node ${START_FRONTEND} ${testDir}`);
+async function executeCLI(inputs: string[], delay = 500) {
+  const cliProcess = exec(`node ${START_FRONTEND} ${testDir}`);
 
-//   function nextPrompt(inputs: string[]) {
-//     if (!inputs.length) return;
+  function nextPrompt(inputs: string[]) {
+    if (!inputs.length) return;
 
-//     // Write the input to the CLI process with a delay
-//     setTimeout(() => {
-//       cliProcess?.stdin?.write(inputs[0]);
-//       nextPrompt(inputs.slice(1));
-//     }, delay);
-//   }
+    // Write the input to the CLI process with a delay
+    setTimeout(() => {
+      cliProcess?.stdin?.write(inputs[0]);
+      nextPrompt(inputs.slice(1));
+    }, delay);
+  }
 
-//   nextPrompt(inputs);
+  nextPrompt(inputs);
 
-//   return new Promise((resolve) => cliProcess.on("exit", resolve));
-// }
+  return new Promise((resolve) => cliProcess.on("exit", resolve));
+}
 
 describe("start-frontend", () => {
   beforeAll(() => {
@@ -50,9 +51,9 @@ describe("start-frontend", () => {
       // Use the default GitHub Actions temporary directory for development in the CI environment.
       // refs: https://docs.github.com/en/actions/learn-github-actions/variables#default-environment-variables
       // eslint-disable-next-line turbo/no-undeclared-env-vars
+      // path: /home/runner/work/_temp
       process.env.RUNNER_TEMP ||
       execSync("mktemp -d -t my-test").toString("utf-8");
-    cleanupTestDir();
   });
 
   afterAll(cleanupTestDir);
@@ -91,104 +92,104 @@ describe("start-frontend", () => {
       --version, -v       Show the version of this script`);
   });
 
-  //   test(
-  //     "handle interactive configuration on CLI",
-  //     async () => {
-  //       await executeCLI([
-  //         // Which location you want to start project?
-  //         KEY.ENTER,
-  //         // Select a JavsScript library for UI
-  //         KEY.ENTER,
-  //         // Select an API Solution (Use arrow keys)
-  //         KEY.ENTER,
-  //         // Select module do you want to use (Press 'space' to select)
-  //         KEY.ENTER,
-  //         // Add Testing codes for Catching bugs early?
-  //         KEY.ENTER,
-  //         // Add Vitest for Unit Testing?
-  //         KEY.ENTER,
-  //         // Add Storybook for Visual Testing?
-  //         KEY.ENTER,
-  //         // Add Playwright for End-To-End Testing?
-  //         KEY.ENTER,
-  //         // Add ESLint for Code Linting?
-  //         KEY.ENTER,
-  //         // Add Prettier for Code Formatting?
-  //         KEY.ENTER,
-  //       ]);
+  test(
+    "handle interactive configuration on CLI",
+    async () => {
+      await executeCLI([
+        // Which location you want to start project?
+        KEY.ENTER,
+        // Select a JavsScript library for UI
+        KEY.ENTER,
+        // Select an API Solution (Use arrow keys)
+        KEY.ENTER,
+        // Select module do you want to use (Press 'space' to select)
+        KEY.ENTER,
+        // Add Testing codes for Catching bugs early?
+        KEY.ENTER,
+        // Add Vitest for Unit Testing?
+        KEY.ENTER,
+        // Add Storybook for Visual Testing?
+        KEY.ENTER,
+        // Add Playwright for End-To-End Testing?
+        KEY.ENTER,
+        // Add ESLint for Code Linting?
+        KEY.ENTER,
+        // Add Prettier for Code Formatting?
+        KEY.ENTER,
+      ]);
 
-  //       // Execute tree-cli to get the directory structure and convert it to a string
-  //       const result = execSync(
-  //         `npx tree-cli -a -l 5 --base ${testDir}`
-  //       ).toString("utf-8");
-  //       expect(result).toContain(`
-  // ├── .eslintignore
-  // ├── .eslintrc.cjs
-  // ├── .gitignore
-  // ├── .prettierignore
-  // ├── .prettierrc
-  // ├── .storybook
-  // |  ├── main.ts
-  // |  ├── preview-head.html
-  // |  └── preview.ts
-  // ├── __mocks__
-  // |  ├── browser.ts
-  // |  ├── index.ts
-  // |  ├── request-handlers.ts
-  // |  └── server.ts
-  // ├── __tests__
-  // |  ├── About.test.ts
-  // |  ├── Home.test.ts
-  // |  └── utils
-  // |     └── global-setup.ts
-  // ├── env.d.ts
-  // ├── index.html
-  // ├── package.json
-  // ├── playwright.config.ts
-  // ├── public
-  // |  ├── favicon.svg
-  // |  └── mockServiceWorker.js
-  // ├── src
-  // |  ├── app.tsx
-  // |  ├── assets
-  // |  |  ├── base.css
-  // |  |  └── main.css
-  // |  ├── components
-  // |  |  ├── Button.tsx
-  // |  |  └── button.module.css
-  // |  ├── context.tsx
-  // |  ├── main.tsx
-  // |  ├── modules
-  // |  |  └── restful
-  // |  |     ├── components
-  // |  |     |  ├── user-form.test.tsx
-  // |  |     |  ├── user-form.tsx
-  // |  |     |  ├── user-list.test.tsx
-  // |  |     |  ├── user-list.tsx
-  // |  |     |  ├── user-view.test.tsx
-  // |  |     |  └── user-view.tsx
-  // |  |     ├── hooks
-  // |  |     |  ├── use-user.test.tsx
-  // |  |     |  └── use-user.ts
-  // |  |     └── index.ts
-  // |  ├── routes.tsx
-  // |  └── ui
-  // |     ├── nav-link.tsx
-  // |     └── pages
-  // |        ├── about
-  // |        |  └── index.tsx
-  // |        ├── index.tsx
-  // |        ├── layout.tsx
-  // |        └── not-found
-  // |           └── index.tsx
-  // ├── stories
-  // |  └── Button.stories.tsx
-  // ├── tsconfig.json
-  // ├── tsconfig.node.json
-  // ├── vite.config.ts
-  // ├── vitest.config.ts
-  // └── vitest.setup.ts`);
-  //     },
-  //     INTERACTIVE_TEST_TIMEOUT
-  //   );
+      // Execute tree-cli to get the directory structure and convert it to a string
+      const result = execSync(
+        `npx tree-cli -a -l 5 --base ${testDir}`
+      ).toString("utf-8");
+      expect(result).toContain(`
+├── .eslintignore
+├── .eslintrc.cjs
+├── .gitignore
+├── .prettierignore
+├── .prettierrc
+├── .storybook
+|  ├── main.ts
+|  ├── preview-head.html
+|  └── preview.ts
+├── __mocks__
+|  ├── browser.ts
+|  ├── index.ts
+|  ├── request-handlers.ts
+|  └── server.ts
+├── __tests__
+|  ├── About.test.ts
+|  ├── Home.test.ts
+|  └── utils
+|     └── global-setup.ts
+├── env.d.ts
+├── index.html
+├── package.json
+├── playwright.config.ts
+├── public
+|  ├── favicon.svg
+|  └── mockServiceWorker.js
+├── src
+|  ├── app.tsx
+|  ├── assets
+|  |  ├── base.css
+|  |  └── main.css
+|  ├── components
+|  |  ├── Button.tsx
+|  |  └── button.module.css
+|  ├── context.tsx
+|  ├── main.tsx
+|  ├── modules
+|  |  └── restful
+|  |     ├── components
+|  |     |  ├── user-form.test.tsx
+|  |     |  ├── user-form.tsx
+|  |     |  ├── user-list.test.tsx
+|  |     |  ├── user-list.tsx
+|  |     |  ├── user-view.test.tsx
+|  |     |  └── user-view.tsx
+|  |     ├── hooks
+|  |     |  ├── use-user.test.tsx
+|  |     |  └── use-user.ts
+|  |     └── index.ts
+|  ├── routes.tsx
+|  └── ui
+|     ├── nav-link.tsx
+|     └── pages
+|        ├── about
+|        |  └── index.tsx
+|        ├── index.tsx
+|        ├── layout.tsx
+|        └── not-found
+|           └── index.tsx
+├── stories
+|  └── Button.stories.tsx
+├── tsconfig.json
+├── tsconfig.node.json
+├── vite.config.ts
+├── vitest.config.ts
+└── vitest.setup.ts`);
+    },
+    INTERACTIVE_TEST_TIMEOUT
+  );
 });
